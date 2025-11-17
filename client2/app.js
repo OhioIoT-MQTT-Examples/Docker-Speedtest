7874204b857ff26189dc93bf99fcea40565fc070
Larry Vincek <larry@vincek.com>
2025-11-17 02:19:53 -0700

    replaced IP addresses with placeholders

--- client2/app.js
+++ client2/app.js
@@ -1,7 +1,7 @@
 
 const mqtt = require('mqtt');
 
-const options = { host: '192.168.0.69' };	// <-- set the IP address of your Pi
+const options = { host: '[your Pi IP address]' };
 
 const connection = mqtt.connect(options);
 
