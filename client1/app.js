bfccd87b032eadc85ddcf4823bb78e88faeaefc2
Larry Vincek <larry@vincek.com>
2025-11-17 02:25:41 -0700

    cosmetic edits

--- client1/app.js
+++ client1/app.js
@@ -13,21 +13,18 @@ connection.on('connect', function() {
 connection.subscribe('topic2');
 
 let cnt = 0;
-
 const cycles = 1000;
-
 const start = new Date();
 
 connection.on('message', function(topic, payload) {
 	
-	cnt++;
-
-	if (cnt == cycles) {
+	if (++cnt == cycles) {
 		const stop = new Date();
 		console.log("time: "+(stop - start));
 		process.exit(0);
 	} else {
 		connection.publish("topic1", "payload");
 	}
+
 });
 
